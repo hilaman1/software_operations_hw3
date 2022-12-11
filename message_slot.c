4e42fd486e8acd42f7c0bc93a821905ef2007411
hilaman1 <hilaman1@mail.tau.ac.il>
2022-12-11 11:44:38 +0200

    Initial commit

---
+++ message_slot.c
@@ -0,0 +1,5 @@
+//
+// Created by hillu on 11/12/2022.
+//
+
+#include "message_slot.h"
---
+++ message_slot.h
@@ -0,0 +1,8 @@
+//
+// Created by hillu on 11/12/2022.
+//
+
+#ifndef HW3_MESSAGE_SLOT_H
+#define HW3_MESSAGE_SLOT_H
+
+#endif //HW3_MESSAGE_SLOT_H
