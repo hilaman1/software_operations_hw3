930a4ac117a3cdb0c1d987b2173f27a069ecd8e2
hilaman1 <hilaman1@mail.tau.ac.il>
2022-12-13 12:43:43 +0200

    12:43

--- message_slot.c
+++ message_slot.c
@@ -4,7 +4,7 @@
 #undef MODULE
 #define MODULE
 
-// this code is based on "chardev" files from recitation 6
+// this code is based on CHARDEV1, CHARDEV2 files from recitation 6
 
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
@@ -14,8 +14,20 @@
 
 MODULE_LICENSE("GPL");
 
-static int major = 235;
-#define DEVICE_RANGE_NAME "char_dev"
+
+struct chardev_info {
+    spinlock_t lock;
+};
+
+// used to prevent concurent access into the same device
+static int dev_open_flag = 0;
+
+static struct chardev_info device_info;
+
+// The message the device will give when asked
+static char the_message[BUF_LEN];
+
+// major num is always defined as 235
 
 //================== DEVICE FUNCTIONS ===========================
 static int device_open( struct inode* inode,
@@ -35,7 +47,59 @@ static int device_open( struct inode* inode,
     spin_unlock_irqrestore(&device_info.lock, flags);
     return SUCCESS;
 }
+//---------------------------------------------------------------
+// a process which has already opened
+// the device file attempts to read from it
+static ssize_t device_read( struct file* file,
+                            char __user* buffer,
+                            size_t       length,
+                            loff_t*      offset )
+{
+    // read doesnt really do anything (for now)
+    printk( "Invocing device_read(%p,%ld) - "
+            "operation not supported yet\n"
+            "(last written - %s)\n",
+            file, length, the_message );
+    //invalid argument error
+    return errno;
+}
 
+//---------------------------------------------------------------
+// a processs which has already opened
+// the device file attempts to write to it
+static ssize_t device_write( struct file*       file,
+                             const char __user* buffer
+                             size_t             length,
+                             loff_t*            offset)
+{
+    ssize_t i;
+    printk("Invoking device_write(%p,%ld)\n", file, length);
+    for(i = 0; i < length && i < BUF_LEN; ++i ) {
+        get_user(the_message[i], &buffer[i]);
+    }
+
+    // return the number of input characters used
+    return i;
+}
+//----------------------------------------------------------------
+static long device_ioctl( struct   file* file,
+                          unsigned int   ioctl_command_id,
+                          unsigned long  ioctl_param )
+{
+    // Switch according to the ioctl called
+    if( MSG_SLOT_CHANNEL == ioctl_command_id ) {
+        if (ioctl_param == 0){
+            errno = EINVAL;
+            return -1;
+        }
+        return SUCCESS;
+
+    } else{
+        errno = EINVAL;
+        return -1;
+    }
+
+}
 //==================== DEVICE SETUP =============================
 struct file_operations Fops = {
         .owner	  = THIS_MODULE,
@@ -51,11 +115,6 @@ static int __init message_slot_init(void)
     // init dev struct
     memset( &device_info, 0, sizeof(struct chardev_info) );
     spin_lock_init( &device_info.lock );
-
-    // major num is always defined as 235
-    static int major = 235;
-
-
     return 0;
 }
 
@@ -66,7 +125,10 @@ static void __exit message_slot_cleanup(void)
     unregister_chrdev(major, DEVICE_RANGE_NAME);
 }
 
+//---------------------------------------------------------------
+
 if (module_init(simple_init) != 0) {
     printk(KERN_ERR "message_slot module initialization failed");
 }
+
 module_exit(simple_cleanup);
--- message_slot.h
+++ message_slot.h
@@ -1,8 +1,16 @@
-//
-// Created by hillu on 11/12/2022.
-//
+// based on "chardev.h" from CHARDEV2 dir from recitation 6
+#ifndef CHARDEV_H
+#define CHARDEV_H
 
-#ifndef HW3_MESSAGE_SLOT_H
-#define HW3_MESSAGE_SLOT_H
+#include <linux/ioctl.h>
+#define MAJOR_NUM 235
 
-#endif //HW3_MESSAGE_SLOT_H
+// Set the message of the device driver
+#define IOCTL_SET_ENC _IOWR(MAJOR_NUM, 0, unsigned long)
+
+#define DEVICE_RANGE_NAME "message_slot"
+#define BUF_LEN 80
+#define DEVICE_FILE_NAME "simple_char_dev"
+#define SUCCESS 0
+
+#endif
